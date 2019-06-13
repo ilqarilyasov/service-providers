2c7bb47e9692b2a27a56bd0cf04f237648ed111a
ilqar ilyasov <ilqar_ilyasov@yahoo.com>
2019-06-13 14:00:35 -0500

    Make outlet connections

--- ServiceProviders/Stroyboards/ServiceProviderDetailViewController.swift
+++ ServiceProviders/Stroyboards/ServiceProviderDetailViewController.swift
@@ -10,21 +10,12 @@ import UIKit
 
 class ServiceProviderDetailViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    @IBOutlet weak var serviceProviderMapView: MKMapView!
+    @IBOutlet weak var serviceProviderNameLabel: UILabel!
+    @IBOutlet weak var serviceProviderAddressLabel: UILabel!
+    @IBOutlet weak var reviewCountLabel: UILabel!
+    @IBOutlet weak var overallGradeLabel: UILabel!
+    
 
 }
